bae8ca47279d7b5ce1d06995ad5975826172639e
Schwedler123 <tiagodospvp@outlook.com>
2022-12-14 19:02:06 -0300

    foi retirado a imagem como background

--- src/Pages/ProfileGamer/ProfileGamer.jsx
+++ src/Pages/ProfileGamer/ProfileGamer.jsx
@@ -18,9 +18,6 @@ export function ProfileGamer() {
   }, [])
   return (
     <div className='container-principal' >
-      <div className='container-img' >
-        <img className='background-img' src="https://lh3.googleusercontent.com/Ia8C8krP5UgoanDp38Lt5m_thNo0bEPtij6VL2mIHf2j-EYSGNb0BU4R9UwoWc1BRjs4BfWuUHHZhyFp_86COAvVcrsEtFwPMrHodkXdS3FFysisg5WRfKgY4ebAqzFhaAYMPE80j70mvX5JxePKsPzOeBUM3Y1s2yhfDylflhhrePXeuQDYj8MWuGlRXaKSSWBBTWu0rD5nVukPbhP5hd_6JnfqHyTMOPboGMCHeWaUWzKddeuQiv9KyD50bAqAS7E26KxjRoDPUvSX0OMZ5iUaUY95FpE5TdzFkSS0sShmpwpwuvSIsHfbOKGTwl6npFC-Ey__pO1h4V23LTAwtsVCNLemCe46_wZMNh_FOQ-7E18JlIh6CvAvWDDu5dh2tgRa6ELNxMu_so15bUnM6Q7qjgUGS5lmLYLuKJd8x3eDLPPzsnipDQAdDRo9l8QMA74JFc0w7CRkYbwGakkj-IGUHXEdeWYLIer-H6BjTOGbwx6njRoQlhzIDARmHMVAH3L_pAJ3I28XCzX4yicOe_TQhl5HU9PoQOs7XWQBzGRVZ-Co-SrEAP0SMSxCQ0kbgNfXJyPI7EutAhVubSXkLpSp3XmR4vAaqXrOc2yH8YEH3eBbHwLCQV88NkRd0i5KWiN3LB35dNxxQVU8n0o-6W7IwLlaqzITC7KN7hTbmb7CFOKtdtBuS-OvCf5y9QVbRZRKz1JRUO4qZoYeNhs4BS9ckloWBV3UqUJd08DnTw6IVVbI-Ps6TEj4xha6KNXzeveDXQaB75UV4vtfxej4OlWrwzUlr7xHpbk1MCyF3zsN60LlLD3B4BGOZiCP9IHZFfLr5XscakYmqSbukDSi-6kw4Dj9N06pyt-xra9BGxQTCd7n4sn-Ywp4mSafJFmbic9DtoVeiG2ERjx5xyT-fp7m0Dpd_9EW-s4UN477Rie5A4H4ewz748cqMqkVI0D1crvL6jQ01Kl0siXVi8uIN8IBx_CoPQL_rfSketl-naDrLxAJC2KF670kiA=w1271-h667-no?authuser=0" />
-      </div>
       <section className='section-principal' >
         <div className='container-header' >
           <header className='header-container' >
